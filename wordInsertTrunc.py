7de8de225986fee0e6fa18cbabaad432d8ec9898
chrisdrymon <p4{u3QCEpH1nd7kg',b6>
2019-11-07 17:12:35 -0600

    Continued lowering book count as books were properly fixed.

--- wordInsertTrunc.py
+++ wordInsertTrunc.py
@@ -1,31 +1,9 @@
 import xml.etree.cElementTree as Et
 import os
 
-booknames = ['Acts',
-             'Romans',
-             '1Corinthians',
-             '2Corinthians',
-             'Galatians',
-             'Ephesians',
-             'Philippians',
-             'Colossians',
-             '1Thessalonians',
-             '2Thessalonians',
-             '1Timothy',
-             '2Timothy',
-             'Titus',
-             'Philemon',
-             'Hebrews',
-             'James',
-             '1Peter',
-             '2Peter',
-             '1John',
-             '2John',
-             '3John',
-             'Jude',
-             'Revelation']
+booknames = ['Revelation']
 
-i = 5
+i = 27
 
 for book in booknames:
 
